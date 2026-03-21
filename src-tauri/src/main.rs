19549cb85e0da73ec9425e5feecede8750db2cc4
PGD <guojinbo618@gmail.com>
2026-03-21 19:09:22 +0800

    feat: 初始化项目

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    golden_nanmu_wood_music_lib::run()
+}
